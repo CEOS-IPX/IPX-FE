b9b732e729ab975d438653131b12c8665c186bbd
girimNam <rlfla@naver.com>
2026-07-01 09:43:52 +0900

    chore: SEO 설정 이동 및 수정

---
+++ src/app/providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { reissue } from "@/api/auth";
+import { useAuthStore } from "@/store/authStore";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const setAccessToken = useAuthStore((s) => s.setAccessToken);
+
+  useEffect(() => {
+    // 앱 시작(새로고침 포함)마다 RT 쿠키로 AT 재발급 시도
+    reissue()
+      .then((res) => setAccessToken(res.data.accessToken))
+      .catch(() => {});
+  }, [setAccessToken]);
+
+  return <>{children}</>;
+}
