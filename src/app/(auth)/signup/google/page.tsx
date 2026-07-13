6f808bda7f9b742864e9d9dd9d0d210c59ac2514
minsxo <kimminseo4216@gmail.com>
2026-07-14 02:35:37 +0900

    feat: 구글 회원가입 추가 정보 페이지 기본 구조 생성

---
+++ src/app/(auth)/signup/google/page.tsx
@@ -0,0 +1,7 @@
+export default function GoogleSignupPage() {
+  return (
+    <div className="flex max-w-125 flex-1 flex-col items-start justify-center gap-10 self-stretch">
+      <h1 className="w-full text-headline-emphasis-28 text-title-primary">회원정보</h1>
+    </div>
+  );
+}
